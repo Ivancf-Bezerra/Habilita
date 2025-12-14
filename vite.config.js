0578e45d9cb31cd3c5cbe60df2a49ecf4aed9022
Ivan Bezerra <ivancf.bezerra@gmail.com>
2025-12-14 15:37:18 -0300

    Versão inicial do projeto Vite

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/DASETAAI/',
+  base: '/Habilita/',  // <- nome do repositório
+  plugins: [react()]
 })
